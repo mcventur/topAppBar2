2f8e48b4b984242facb3726e375e2d8d9617a724
mcventur <marcelinopenide@gmail.com>
2023-11-26 21:39:06 +0100

    Seguimos

--- app/src/main/java/com/mpd/pmdm/toppappbar2/MainActivity.kt
+++ app/src/main/java/com/mpd/pmdm/toppappbar2/MainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.mpd.pmdm.toppappbar2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,6 +20,12 @@ class MainActivity : AppCompatActivity() {
 
         //Seteamos la toolbar como Action Bar
         setSupportActionBar(binding.topAppBar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
 
 
